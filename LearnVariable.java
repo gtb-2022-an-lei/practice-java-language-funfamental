0b77211dc30674519c8a4332aed6911f4d9d3170
anlei <287729654@qq.com>
2022-05-10 10:53:54 +0800

    定义int类型变量x，并赋予初始值100

---
+++ LearnVariable.java
@@ -0,0 +1,7 @@
+public class LearnVariable {
+        public static void main(String[] args) {
+            int x = 100; // 定义int类型变量x，并赋予初始值100
+            System.out.println(x); // 打印该变量的值
+        }
+}
+
